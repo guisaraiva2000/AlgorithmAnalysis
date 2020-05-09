6c174f999aad03f5c9b5afdab1d449b897d5ab01
guisaraiva2000 <guilherme.a.saraiva@tecnico.ulisboa.pt>
2020-05-09 17:09:46 +0100

    9 moosh still bad output

--- main.cpp
+++ main.cpp
@@ -1,47 +1,34 @@
 #include <iostream>
-#include <list>
+#include <vector>
 #include <queue>
-#include <string.h>
 #include <climits>
 
 using namespace std;
 
 typedef struct Node{
-    int id, adj_counter, pi;
-    bool isShop, isCitizen, visited;
+    int adj_counter;
+    bool isShop, isCitizen;
     int *adj;
 } node;
 
 int shops, citizens, avenues, streets, V;
-int *cit;
-int *sh;
 int **mat;
 node *vertices;
 
 void make_cap_mat(){
-    V = avenues * streets + 2;
-
-    vertices = new node[V];
 
     mat = new int*[V];
     for(int u = 0; u < V; u++)
         mat[u] = new int[V];
 
-    int i;
-    for(i = 0; i < V; i++){
-        vertices[i].id = i;
-        vertices[i].adj = new int[V - 2];
-        vertices[i].adj_counter = 0;
-        vertices[i].isShop = false;
-        vertices[i].isCitizen = false;
-        vertices[i].visited = false;
-    }
-
     int count, lat = 1;
-    for(i = 1; i < V - 1; i++){
+    for(int i = 1; i < V - 1; i++){
         count = 0;
         if(i >= lat + avenues)
             lat += avenues;
+
+        if (vertices[i].isShop) vertices[i].adj = new int[5];
+        else vertices[i].adj = new int[4];
 
         if(i + 1 < lat + avenues) {
             vertices[i].adj[count++] = i + 1;
@@ -61,135 +48,153 @@ void make_cap_mat(){
         }
         vertices[i].adj_counter = count;
     }
-}
 
-void process_input(){
-
-    scanf( "%d %d", &avenues, &streets);
-    scanf( "%d %d", &shops, &citizens);
-
-    make_cap_mat();
-
-    sh = new int[shops];
-    cit = new int[citizens];
-
-    int x, y, n;
-    for(int i = 0; i < shops; i++){
-        scanf("%d %d", &x, &y);
-        n = (y-1) * avenues + x;
-        sh[i] = n;
-        vertices[n].isShop = true;
-    }
-    for(int i = 0; i < citizens; i++){
-        scanf("%d %d", &x, &y);
-        n = (y-1) * avenues + x;
-        cit[i] = n;
-        vertices[n].isCitizen = true;
-    }
-
-    for(int i = 1; i < V; i++){
+    for(int i = 1; i < V-1; i++) {
         if (vertices[i].isCitizen) {
             mat[0][i] = 1;
             vertices[0].adj[vertices[0].adj_counter++] = i;
         }
         if(vertices[i].isShop) {
-            mat[i][V-1] = 1;
-            vertices[i].adj[vertices[i].adj_counter++] = V-1;
+            mat[i][V - 1] = 1;
+            vertices[i].adj[vertices[i].adj_counter++] = V - 1;
         }
     }
-
 }
 
-/* Returns true if there is a path from source 's' to sink 't' in
-  residual graph. Also fills parent[] to store the path */
-bool bfs(int s, int t, int parent[])
-{
-    // Create a visited array and mark all vertices as not visited
-    bool visited[V];
-    memset(visited, 0, sizeof(visited));
+void process_input(){
 
-    // Create a queue, enqueue source vertex and mark source vertex
-    // as visited
+    cin >> avenues >> streets;
+    cin >> shops >> citizens;
+
+    V = avenues * streets + 2;
+
+    vertices = new node[V];
+
+    for(int i = 0; i < V; i++){
+        vertices[i].adj_counter = 0;
+        vertices[i].isShop = false;
+        vertices[i].isCitizen = false;
+    }
+
+    vertices[0].adj = new int[citizens];
+
+    int x, y, n;
+    for(int i = 0; i < shops; i++){
+        cin >> x >> y;
+        n = (y-1) * avenues + x;
+        vertices[n].isShop = true;
+    }
+    for(int i = 0; i < citizens; i++){
+        cin >> x >> y;
+        n = (y-1) * avenues + x;
+        vertices[n].isCitizen = true;
+    }
+
+    make_cap_mat();
+}
+
+bool bfs(int s, int t, int* parent){
+    vector<bool> visited;
+    visited.reserve(V);
+
     queue <int> q;
     q.push(s);
     visited[s] = true;
     parent[s] = -1;
     int v;
 
-    // Standard BFS Loop
-    while (!q.empty())
-    {
+    while (!q.empty()){
         int u = q.front();
         q.pop();
 
         for (int i = 0; i < vertices[u].adj_counter; i++){
             v = vertices[u].adj[i];
-            if (!visited[v]  && mat[u][v] > 0) {
-                q.push(v);
+            if (!visited[v] && mat[u][v] > 0) {
                 parent[v] = u;
                 visited[v] = true;
+                if(v == u) return true;
+                q.push(v);
             }
         }
     }
-    // If we reached sink in BFS starting from source, then return
-    // true, else false
     return visited[t];
 }
 
-int findDisjointPaths(int s, int t)
-{
+int findDisjointPaths(int s, int t){
     int u, v;
+    int *parent = new int[V];
+    int max_flow = 0;
 
-    int parent[V];  // This array is filled by BFS and to store path
-
-    int max_flow = 0;  // There is no flow initially
-
-    // Augment the flow while tere is path from source to sink
-    while (bfs(s, t, parent))
-    {
-        // Find minimum residual capacity of the edges along the
-        // path filled by BFS. Or we can say find the maximum flow
-        // through the path found.
+    while (bfs(s, t, parent)){
         int path_flow = INT_MAX;
 
-        for (v=t; v!=s; v=parent[v])
-        {
+        for (v=t; v!=s; v=parent[v]) {
             u = parent[v];
             path_flow = min(path_flow, mat[u][v]);
         }
 
-        // update residual capacities of the edges and reverse edges
-        // along the path
-        for (v=t; v != s; v=parent[v])
-        {
+        for (v=t; v != s; v=parent[v]) {
             u = parent[v];
             mat[u][v] -= path_flow;
             mat[v][u] += path_flow;
         }
-
-        // Add path flow to overall flow
         max_flow += path_flow;
     }
-
-    // Return the overall flow (max_flow is equal to maximum
-    // number of edge-disjoint paths)
+    delete [] parent;
     return max_flow;
 }
 
-void print_mat(){
+/*void print_mat(){
     int i,j;
     for(i = 0; i < V ; i++) {
+        cout << "{";
         for (j = 0; j < V; j++)
-            cout << mat[i][j] << " - ";
-        cout << "\n";
+            cout << mat[i][j] << ",";
+        cout << "}," << endl;
     }
-}
+}*/
 
 int main() {
     process_input();
 
-    cout << findDisjointPaths(0, V-1);
+    cout << findDisjointPaths(0, V-1) << endl;
     //print_mat();
+
+    for(int u = 0; u < V; u++)
+        delete[] mat[u];
+    delete[] mat;
+
+    for(int i = 0; i < V ; i++)
+        delete[] vertices[i].adj;
+    delete[] vertices;
 
     return 0;
 }
+ /*
+           {{0,0,1,0,0,0,0,1,1,0,0,0,1,0,0,1,0,0,0,1,0,0,1,0,0,0,0},
+            {0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+            {0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+            {0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,1},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};*/
